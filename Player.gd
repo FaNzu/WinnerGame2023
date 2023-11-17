7baee6727297ba7547fbe25413ebc9cb314032cb
Daniel Hoppe Hansen <danielhoppe123@gmail.com>
2023-11-17 21:00:48 +0100

    Added idle animations and moving animations when input is given.

--- Player.gd
+++ Player.gd
@@ -7,6 +7,25 @@ func _ready():
 	screen_size = get_viewport_rect().size
 	
 func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+		$AnimatedSprite2D.play("walkright")
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+		$AnimatedSprite2D.play("walkleft")
+	if Input.is_action_pressed("move_up"):
+		velocity.y += 1
+		$AnimatedSprite2D.play("walkup")
+	if Input.is_action_pressed("move_down"):
+		velocity.y -= 1
+		$AnimatedSprite2D.play("walkdown")
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	else:
+		$AnimatedSprite2D.play("idle")
+	
 	pass
 	# Check for input
 	# Move in the given direction.
