84e4cbf0d4bc9d5210c8950b2413425a68914946
cvHanky <145098299+cvHanky@users.noreply.github.com>
2023-11-18 17:04:22 +0100

    m

--- Ralf.gd
+++ Ralf.gd
@@ -1,15 +1,27 @@
 extends CharacterBody2D
 
 var myTime : float
+@export var speed : float
+var reachedX : bool = false
+var reachedX2 : bool = false
 
 func _physics_process(delta):
-	myTime =  myTime + delta
-	if myTime < 2:
-		move_and_collide(Vector2(1,0))
-		$AnimatedSprite2D.play("walk_right")
-	if myTime >= 2:
-		move_and_collide(Vector2(-1,0))
-		$AnimatedSprite2D.play("walk_left")
-	if myTime >= 4:
+	myTime += delta
+	if myTime < 5:
+		if reachedX == false:
+			move_and_collide(Vector2(speed,0))
+			$AnimatedSprite2D.play("walk_right")
+			if (position.x > 140):
+				reachedX = true
+				reachedX2 = false
+		if reachedX2 == false:
+			move_and_collide(Vector2(-speed,0))
+			$AnimatedSprite2D.play("walk_left")
+			if (position.x < 2):
+				reachedX2 = true
+				reachedX = false
+	if myTime >= 5:
+		$AnimatedSprite2D.stop()
+	if myTime > 15:
 		myTime = 0
 
