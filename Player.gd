770bcef524e7f13f4da68840b66d26eeb8c408ce
Ask <34611138+FaNzu@users.noreply.github.com>
2023-11-18 20:28:06 +0100

    færdigt spil

--- Player.gd
+++ Player.gd
@@ -1,12 +1,17 @@
 extends CharacterBody2D
 
 @export var speed : float  # How fast the player moves
+@onready var actionable_finder: Area2D = $direction/Actionablefinder
+@onready var cameraNode : Camera2D = get_node("Camera2D")
 var screen_size
 var Cvelocity
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	
+func _process(delta):
+	cameraNode.position = position * delta
+
 func _physics_process(delta):
 	Cvelocity = 0
 	if Input.is_action_pressed("move_right"):
@@ -31,3 +36,9 @@ func _physics_process(delta):
 		
 	if Cvelocity == 0:
 		$AnimatedSprite2D.play("idle")
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		var actionables = actionable_finder.get_overlapping_areas()
+		if actionables.size() > 0:
+			actionables[0].action()
+			return
