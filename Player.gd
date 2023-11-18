26f0cb63c1566d7ff90b6aab7a84909d52280eda
cvHanky <145098299+cvHanky@users.noreply.github.com>
2023-11-18 15:31:33 +0100

    Added collision to the player

--- Player.gd
+++ Player.gd
@@ -1,29 +1,33 @@
-extends Area2D
+extends CharacterBody2D
 
-@export var speed = 400   # How fast the player moves
+@export var speed : float  # How fast the player moves
 var screen_size
+var Cvelocity
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	
-func _process(delta):
-	var velocity = Vector2.ZERO
+func _physics_process(delta):
+	Cvelocity = 0
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 50
+		move_and_collide(Vector2(speed,0))
 		$AnimatedSprite2D.play("walkright")
+		Cvelocity += 1
+		
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 50
+		move_and_collide(Vector2(-speed,0))
 		$AnimatedSprite2D.play("walkleft")
+		Cvelocity += 1
+		
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 50
+		move_and_collide(Vector2(0,-speed))
 		$AnimatedSprite2D.play("walkup")
+		Cvelocity += 1
+		
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 50
+		move_and_collide(Vector2(0,speed))
 		$AnimatedSprite2D.play("walkdown")
-	position += delta * velocity
-	
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-	else:
+		Cvelocity += 1
+		
+	if Cvelocity == 0:
 		$AnimatedSprite2D.play("idle")
-		pass
