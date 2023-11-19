8ea40fe55d96682598cfb3e7157261eb62c80c03
cvHanky <145098299+cvHanky@users.noreply.github.com>
2023-11-19 10:32:34 +0100

    updated ralf
    skrt

--- Ralf.gd
+++ Ralf.gd
@@ -5,27 +5,51 @@ var myTime : float
 var reachedX : bool = false
 var reachedX2 : bool = false
 @onready var Actionablef : Area2D = $Actionable
+var walkingState : String = "LEFT"
 
 func _process(delta):
 	Actionablef.position = position * delta
 
 func _physics_process(delta):
-	myTime += delta
-	if myTime < 5:
-		if reachedX == false:
-			move_and_collide(Vector2(speed,0))
-			$AnimatedSprite2D.play("walk_right")
-			if (position.x > 140):
-				reachedX = true
-				reachedX2 = false
-		if reachedX2 == false:
-			move_and_collide(Vector2(-speed,0))
-			$AnimatedSprite2D.play("walk_left")
-			if (position.x < 2):
-				reachedX2 = true
-				reachedX = false
-	if myTime >= 5:
+	if walkingState == "LEFT":
+		move_and_collide(Vector2(speed,0))
+		$AnimatedSprite2D.play("walk_right")
+		if (position.x > 140):
+			walkingState = "stoppingfirst"
+	if walkingState == "stoppingfirst":
+		myTime += delta
+		$AnimatedSprite2D.play("idle")
+		if (myTime > 20):
+			walkingState = "RIGHT"
+			myTime = 0
+	if walkingState == "RIGHT":
+		move_and_collide(Vector2(-speed,0))
+		$AnimatedSprite2D.play("walk_left")
+		if (position.x < -20):
+			walkingState = "UP"
+	if walkingState == "UP":
+		move_and_collide(Vector2(0,-speed))
+		$AnimatedSprite2D.play("walk_up")
+		if (position.y < -42):
+			walkingState = "LEFTagain"
+	if walkingState == "LEFTagain":
+		move_and_collide(Vector2(-speed,0))
+		$AnimatedSprite2D.play("walk_left")
+		if (position.x < -30):
+			walkingState = "STOP"
+	if walkingState == "STOP":
 		$AnimatedSprite2D.stop()
-	if myTime > 15:
-		myTime = 0
-
+		myTime += delta
+		if myTime > 20:
+			walkingState = "LEFTback"
+			myTime = 0
+	if walkingState == "LEFTback":
+		move_and_collide(Vector2(speed,0))
+		$AnimatedSprite2D.play("walk_left")
+		if (position.x > -20):
+			walkingState = "downBACK"
+	if walkingState == "downBACK":
+		move_and_collide(Vector2(0,speed))
+		$AnimatedSprite2D.play(("walk_down"))
+		if (position.y >= 0):
+			walkingState = "LEFT"
